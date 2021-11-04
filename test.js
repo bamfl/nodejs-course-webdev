71bf3ce3c549cec2bf842009ade4f092fde815cb
Dmitriy Petukhov <pdnsite@gmail.com>
2021-11-04 09:49:59 +0300

    add buffer and steram

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // #1 Базовые концепции и установка (Basic Concepts & Setup Environment)
 const userName = 'Dmitriy';
 
-const sayHi = (name) => `My name is ${name}`;
+const sayHi = name => `My name is ${name}`;
 
 // console.log(sayHi(userName)); // node test.js
 
